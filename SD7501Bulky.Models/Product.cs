e6aa5960299a44bfc479e92880865539830687a6
he1322482639 <he1322482639@gmail.com>
2026-04-09 20:08:28 +1200

    Product class

---
+++ SD7501Bulky.Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace BulkWeb.Models 
+{
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ISBN { get; set; }
+        public string Author { get; set; }
+        public double ListPrice { get; set; }
+        public double Price { get; set; }
+        public double Price50 { get; set; }
+        public double Price100 { get; set; }
+
+    }
+
+}
+
+
+
+
