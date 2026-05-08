1d0bf3b0ca68a5ac114fd96469e790888f670d45
he1322482639 <he1322482639@gmail.com>
2026-05-09 00:04:02 +1200

    Add Order and Summary

--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using SD7501Bulky.DataAccess.Repository.IRepository;
 using SD7501Bulky.Models;
 using SD7501Bulky.Models.ViewModels;
+using SD7501Bulky.Utility;
 using System.Security.Claims;
 
 namespace BulkyWeb.Areas.Customer.Controllers
@@ -11,13 +12,13 @@ namespace BulkyWeb.Areas.Customer.Controllers
     [Authorize]
     public class CartController : Controller
     {
-        private readonly IUnitOfWork _uniOfWork;
+        private readonly IUnitOfWork _unitOfWork;
 
         [BindProperty]
         public ShoppingCartVM ShoppingCartVM { get; set; }
         public CartController(IUnitOfWork uniOfWork)
         {
-            _uniOfWork = uniOfWork;
+            _unitOfWork = uniOfWork;
         }
         public IActionResult Index()
         {
@@ -26,54 +27,103 @@ namespace BulkyWeb.Areas.Customer.Controllers
 
             ShoppingCartVM = new ShoppingCartVM()
             {
-                ShoppingCartList = _uniOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId,
+                ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId,
                 includeProperties: "Product"),
+                OrderHeader = new OrderHeader()
             };
             foreach (var cart in ShoppingCartVM.ShoppingCartList)
             {
                 cart.Price = GetPriceBasedOnQuantity(cart);
-                ShoppingCartVM.OrderTotal += (cart.Price * cart.Count);
+                ShoppingCartVM.OrderHeader.OrderTotal += (cart.Price * cart.Count);
             }
-
+            
             return View(ShoppingCartVM);
 
         }
         public IActionResult Plus(int cartId)
         {
-            var cartFromDb = _uniOfWork.ShoppingCart.Get(u => u.Id == cartId);
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
             cartFromDb.Count += 1;
-            _uniOfWork.ShoppingCart.Update(cartFromDb);
-            _uniOfWork.Save();
+            _unitOfWork.ShoppingCart.Update(cartFromDb);
+            _unitOfWork.Save();
             return RedirectToAction(nameof(Index));
         }
         public IActionResult Minus(int cartId)
         {
-            var cartFromDb = _uniOfWork.ShoppingCart.Get(u => u.Id == cartId);
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
             if (cartFromDb.Count <= 1)
             {
-                _uniOfWork.ShoppingCart.Remove(cartFromDb);
+                _unitOfWork.ShoppingCart.Remove(cartFromDb);
             }
             else
             {
                 cartFromDb.Count -= 1;
-                _uniOfWork.ShoppingCart.Update(cartFromDb);
+                _unitOfWork.ShoppingCart.Update(cartFromDb);
             }
-            _uniOfWork.Save();
+            _unitOfWork.Save();
             return RedirectToAction(nameof(Index));
         }
         public IActionResult Remove(int cartId)
         {
-            var cartFromDb = _uniOfWork.ShoppingCart.Get(u => u.Id == cartId);
-            _uniOfWork.ShoppingCart.Remove(cartFromDb);
-            _uniOfWork.Save();
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+            _unitOfWork.ShoppingCart.Remove(cartFromDb);
+            _unitOfWork.Save();
             return RedirectToAction(nameof(Index));
         }
-        public IActionResult Summary()
+
+        [HttpPost]
+        [ActionName("Summary")] 
+        public IActionResult SummaryPOST()
         {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
 
-            return View();
+            ShoppingCartVM.ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId, 
+                includeProperties: "Product");
+            
+            ShoppingCartVM.OrderHeader.OrderDate = System.DateTime.Now;
+            ShoppingCartVM.OrderHeader.ApplicationUserId = userId;
+
+            ApplicationUser applicationUser = _unitOfWork.ApplicationUser.Get(u => u.Id == userId);
+
+            foreach (var cart in ShoppingCartVM.ShoppingCartList)
+            {
+                cart.Price = GetPriceBasedOnQuantity(cart);
+                ShoppingCartVM.OrderHeader.OrderTotal += (cart.Price * cart.Count);
+            }
+
+            if (applicationUser.CompanyId.GetValueOrDefault() == 0)
+            {
+                ShoppingCartVM.OrderHeader.PaymentStatus = SD.PaymentStatusPending;
+                ShoppingCartVM.OrderHeader.OrderStatus = SD.StatusPending;
+            }
+            else
+            {
+                ShoppingCartVM.OrderHeader.PaymentStatus = SD.PaymentStatusDelayedPayment;
+                ShoppingCartVM.OrderHeader.OrderStatus = SD.StatusApproved;
+            }
+                  
+            _unitOfWork.OrderHeader.Add(ShoppingCartVM.OrderHeader);
+            _unitOfWork.Save();
+
+            foreach (var cart in ShoppingCartVM.ShoppingCartList)
+            {
+                OrderDetail orderDetail = new OrderDetail()
+                {
+                    ProductId = cart.ProductId,
+                    OrderHeaderId = ShoppingCartVM.OrderHeader.Id,
+                    Price = cart.Price,
+                    Count = cart.Count
+                };
+                _unitOfWork.OrderDetail.Add(orderDetail);
+                _unitOfWork.Save();
+            }
+            return RedirectToAction(nameof(OrderConfirmation), new { id = ShoppingCartVM.OrderHeader.Id });
         }
-
+        public IActionResult OrderConfirmation(int id)
+        {
+             return View();
+        }
 
         public double GetPriceBasedOnQuantity(ShoppingCart shoppingCart)
         {
--- SD7501Bulky.DataAccess/Migrations/20260508103121_addOrderHeaderAndDetailsToDb.Designer.cs
+++ SD7501Bulky.DataAccess/Migrations/20260508103121_addOrderHeaderAndDetailsToDb.Designer.cs
@@ -4,6 +4,7 @@ using BulkyWeb.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace SD7501Bulky.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260508103121_addOrderHeaderAndDetailsToDb")]
+    partial class addOrderHeaderAndDetailsToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -609,9 +612,6 @@ namespace SD7501Bulky.DataAccess.Migrations
                     b.Property<string>("City")
                         .HasColumnType("nvarchar(max)");
 
-                    b.Property<int?>("CompanyId")
-                        .HasColumnType("int");
-
                     b.Property<string>("Name")
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
@@ -624,8 +624,6 @@ namespace SD7501Bulky.DataAccess.Migrations
 
                     b.Property<string>("StreetAddress")
                         .HasColumnType("nvarchar(max)");
-
-                    b.HasIndex("CompanyId");
 
                     b.HasDiscriminator().HasValue("ApplicationUser");
                 });
@@ -739,15 +737,6 @@ namespace SD7501Bulky.DataAccess.Migrations
                     b.Navigation("ApplicationUser");
 
                     b.Navigation("Product");
-                });
-
-            modelBuilder.Entity("SD7501Bulky.Models.ApplicationUser", b =>
-                {
-                    b.HasOne("SD7501Bulky.Models.Company", "Company")
-                        .WithMany()
-                        .HasForeignKey("CompanyId");
-
-                    b.Navigation("Company");
                 });
 #pragma warning restore 612, 618
         }
